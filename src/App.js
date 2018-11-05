d636736986fc46cbf473c51de81b5a337b9fe01f
Edward Cheadle <edward.cheadle@cambiahealth.com>
2018-11-04 22:03:35 -0700

    Change app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there! I was changed on the feature branch
+            Hi there! I was changed on the feature branch and again I changed it.
           </p>
           <a
             className="App-link"
